0bacfcf90997823f064d5c7cdee6187c622f8610
krystian-blackboxrealities <krystian@blackboxrealities.com>
2020-01-19 13:25:33 -0600

    project files

---
+++ Assets/Scripts/AnimationStateController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationStateController : MonoBehaviour
+{
+    private Animator targetAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetAnimator = GetComponent<Animator>();
+    }
+
+    
+    public void SetAnimationState(string stateName)
+    {
+        targetAnimator.CrossFade(stateName,0);
+    }
+}
---
+++ Assets/Scripts/FreeLookInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineFreeLook))]
+public class FreeLookInput : MonoBehaviour
+{
+    private CinemachineFreeLook freeLookCamera;
+
+    private string XAxisName = "Mouse X";
+    private string YAxisName = "Mouse Y";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        freeLookCamera = GetComponent<CinemachineFreeLook>();
+        freeLookCamera.m_XAxis.m_InputAxisName = "";
+        freeLookCamera.m_YAxis.m_InputAxisName = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            freeLookCamera.m_XAxis.m_InputAxisValue = Input.GetAxis(XAxisName);
+            freeLookCamera.m_YAxis.m_InputAxisValue = Input.GetAxis(YAxisName);
+        }
+        else
+        {
+            freeLookCamera.m_XAxis.m_InputAxisValue = 0;
+            freeLookCamera.m_YAxis.m_InputAxisValue = 0;
+        }
+    }
+}
